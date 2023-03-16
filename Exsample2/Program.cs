f0215df28e02f66fd6af14d7f6fc70afff9b7ca0
Stanislsav <Sculkoskij@gmail.ru>
2023-03-16 18:01:37 +0300

    Финал

---
+++ Exsample2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine ($" Введите размер массива ");
+int i = Convert.ToInt32 (Console.ReadLine ());
+int[] newArray = newint();
+Console.WriteLine(string.Join(" ", newArray));
+Console.WriteLine($"Количество четных чисел в массиве = {sum()} ");
+
+
+int[] newint()
+{
+     int[] array = new int[i];
+     for (int j = 0; j < i; j++)
+     {
+        array[j] = Random.Shared.Next(1,101);
+     }
+     return array;
+}
+
+int sum()
+{
+    int sum = 0;
+    int []array = newArray;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 ==1)
+    {
+        sum = sum + newArray[i];
+    }
+    }
+    return sum;
+}
---
+++ Exsample3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76\
+
+Console.WriteLine ($" Введите размер массива ");
+int i = Convert.ToInt32 (Console.ReadLine ());
+int[] newArray = newint();
+int Dif = max() - min();
+Console.WriteLine(string.Join(" ", newArray));
+Console.WriteLine($"Разница между максимальным и минимальным значением массива = {Dif} ");
+
+
+int[] newint()
+{
+     int[] array = new int[i];
+     for (int j = 0; j < i; j++)
+     {
+        array[j] = Random.Shared.Next(1,101);
+     }
+     return array;
+}
+
+int max()
+{
+    int Max = 0;
+    int [] array = newArray;
+    for (int j = 0; j < i; j++)
+    {
+        if (array[j] > Max)
+        Max = array[j];
+    }
+    return Max;
+}
+
+int min()
+{
+    int [] array2 = newArray;
+    int Min = array2[0];
+    for (int j = 0; j < i; j++)
+    {
+        if (array2[j] < Min)
+        Min = array2[j];
+    }
+    return Min;
+}
